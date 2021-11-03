018090951b18b63baf7ac92ea15ee31a6374943d
NicholasReardon133 <89750363+NicholasReardon133@users.noreply.github.com>
2021-11-02 21:28:10 -0400

    made some changes to olay file

--- game/play.py
+++ game/play.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import tetris
 
 pygame.init()
 
@@ -15,7 +16,19 @@ screen = pygame.display.set_mode(size)
 done = False
 clock = pygame.time.Clock()
 fps = 25
-game = Tetris(20, 10)
+game = tetris(20, 10)
 counter = 0
 
 pressing_down = False
+
+while (not done):
+    if game.figure is None:
+        game.new_figure()
+
+    if counter % (fps // game.level // 2) == 0 or pressing_down:
+        if game.state == "start":
+            game.go_down()
+
+            
+
+pygame.quit()
