79ae27e22d07da6353dd604b13007076b734c87d
NicholasReardon133 <89750363+NicholasReardon133@users.noreply.github.com>
2021-11-02 20:58:35 -0400

    added a couple of lines to play file

--- game/play.py
+++ game/play.py
@@ -1,2 +1,15 @@
 import pygame
 import random
+
+class playTetris:
+
+    level = 2
+    score = 0
+    state = "start"
+    field = []
+    height = 0
+    width = 0
+    x = 100
+    y = 60
+    zoom = 20
+    figure = None
